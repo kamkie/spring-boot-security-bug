e707c6521f34f3f22879730cb3442472ba634362
Kamil Kiewisz <kamkie@outlook.com>
2019-03-09 20:12:33 +0100

    works now

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # spring mvc vs ant matcher permitall
 
 I found problem in new version of spring boot 2.0.5 with security maters other than mvcMatchers (antMatchers and requestMatchers -> EndpointRequest) when server.servlet.path is set to some path
+
 additionally I found that without using server.servlet.path content negotiation for `/` does not work and controller always wins over static files
+
 example behaves little bit different when mvcMatchers will be replaced with equivalent antMatchers in`SecurityConfig` 
+
 changing version of spring security doesn't help
+
 in spring boot release notes I found this commit https://github.com/spring-projects/spring-boot/commit/b93c2b9a9fba057470b03944cdd59e37f1ebbc59
 
 
--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,20 +1,29 @@
 package com.example.demo.controller;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping(path = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity home() {
-        return ResponseEntity.ok("rest home");
+        return ResponseEntity.ok(Map.of("message", "rest home"));
+    }
+
+    @GetMapping(path = "/", produces = MediaType.TEXT_HTML_VALUE)
+    public ResponseEntity index() {
+        return ResponseEntity.ok(new ClassPathResource("static/index.html"));
     }
 
     @GetMapping("/hello")
     public ResponseEntity hello() {
-        return ResponseEntity.ok("rest hello");
+        return ResponseEntity.ok(Map.of("message", "rest hello"));
     }
 
 }
